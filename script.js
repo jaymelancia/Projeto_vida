b2a1835a37c314dde3983ec87d17d64d6f2c97b2
jaymelancia <laura.bonzanini.vieira@escola.pr.gov.br>
2026-05-18 15:00:19 +0000

    18.05

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao")
+const textos = document.querySelectorAll(".aba-conteudo")
 for (let i=0; i < botoes.length; i++){
     botoes[i].onclick = function () {
         for (let j=0; j<botoes.length; j++){
@@ -6,7 +7,7 @@ for (let i=0; i < botoes.length; i++){
             textos[j].classList.remove("ativo");
         }
         botoes[i].classList.add("ativo")
-        textos[i].classLIst.add("ativo")
+        textos[i].classList.add("ativo")
     }
 console.log(botoes);
 }
